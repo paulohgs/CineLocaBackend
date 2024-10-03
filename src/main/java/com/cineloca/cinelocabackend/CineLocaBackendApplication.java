46c3aa049fae0d6066fbaa132f47452b34aa914b
Paulo Henrique <phsilva098@gmail.com>
2024-10-03 17:37:14 -0300

    First Commit

---
+++ src/main/java/com/cineloca/cinelocabackend/CineLocaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cineloca.cinelocabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineLocaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineLocaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineloca/cinelocabackend/CineLocaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineloca.cinelocabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineLocaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
